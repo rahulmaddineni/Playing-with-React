9170de433594025084f95a4953074462c6731b18
Rahul maddineni <maddineni.rahulbabu@gmail.com>
2017-05-30 02:38:14 -0400

    Changes to UI: - Selecting and deselecting numbers from answer - button disabled on no elements

--- play-nine/src/App.js
+++ play-nine/src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import './index.css';
 import './bootstrap.css';
 var FontAwesome = require('react-fontawesome');
+var _ = require('lodash');
 
 
 const Stars = (props) => {
-  const numberOfStars = 1 + Math.floor(Math.random()*9);
-
-  let stars = [];
-  for(let i=0; i<numberOfStars; i++){
-    stars.push(<FontAwesome key={i} className='fa fa-star'/>)
-  }
   return(
     <div className="col-5">
-      {stars}
+      {_.range(props.numberOfStars).map(i => 
+        <FontAwesome key={i} className='fa fa-star'/>
+      )}
     </div>
   );
 }
@@ -22,7 +19,7 @@ const Stars = (props) => {
 const Button = (props) => {
   return(
     <div className="col-2">
-    <button>=</button>
+    <button className="btn" disabled={props.selectedNumbers.length === 0}>=</button>
     </div>
   );
 }
@@ -30,25 +27,58 @@ const Button = (props) => {
 const Answer = (props) => {
   return(
     <div className="col-5">
-    ...
+    {props.selectedNumbers.map((number,i) =>
+      <span key={i}
+          onClick={() => props.deSelectNumber(number)}>
+        {number}
+      </span>
+    )}
     </div>
   );
 }
 
 const Numbers = (props) => {
-    return(
-      <div className="card text-center">
-        <div>
-          <span className="number">1</span>
-          <span className="number selected">2</span>
-          <span className="number used">3</span>
-        </div>
+  const numberClassName = (number) => {
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+      return 'selected';
+    }
+  };
+  return (
+    <div className="card text-center">
+      <div>
+        {Numbers.list.map((number, i) =>
+          <span key={i} className={numberClassName(number)}
+                onClick={() => props.selectNumber(number)}>
+            {number}
+          </span>
+        )}
       </div>
-    );
-}
+    </div>
+  );
+};
+Numbers.list = _.range(1,10); //loadash functions
 
 class Game extends Component {
+  state = {
+    selectedNumbers: [],
+    randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+  };
+
+  selectNumber = (clickedNumber) =>{
+    if(this.state.selectedNumbers.indexOf(clickedNumber) >= 0) {return;}
+    this.setState(prevState => ({
+      selectedNumbers : prevState.selectedNumbers.concat(clickedNumber)
+    }));
+  }
+
+  deSelectNumber = (clickedNumber) =>{
+    this.setState(prevState => ({
+      selectedNumbers : prevState.selectedNumbers.filter(number => number !== clickedNumber)
+    }));
+  }
   render(){
+    // reducing calls using this.state,selectedNumbers, ...
+    const { selectedNumbers, randomNumberOfStars } = this.state;
     return(
       <div className="App">
         <div className="App-header">
@@ -57,12 +87,14 @@ class Game extends Component {
         <br />
         <br />
         <div className="row">
-          <Stars />
-          <Button />
-          <Answer />          
+          <Stars numberOfStars={randomNumberOfStars}/>
+          <Button selectedNumbers={selectedNumbers}/>
+          <Answer selectedNumbers={selectedNumbers}
+                  deSelectNumber={this.deSelectNumber}/>          
         </div>
         <br />
-        <Numbers />
+        <Numbers selectedNumbers={selectedNumbers}
+                 selectNumber={this.selectNumber}/>
       </div>
     );
   }
